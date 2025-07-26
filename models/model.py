a6ac300042355f187dd38890cb76d3a438bd152c
Abhinav Rajput <abhinavrajput179@gmail.com>
2025-07-26 14:43:16 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -225,37 +225,31 @@ class BiasFree_LayerNorm(nn.Module):
         return x / torch.sqrt(sigma + 1e-5) * self.weight
 
 
-class WithBias_LayerNorm(nn.Module):
-    def __init__(self, normalized_shape):
-        super(WithBias_LayerNorm, self).__init__()
-        if isinstance(normalized_shape, numbers.Integral):
-            normalized_shape = (normalized_shape,)
-        normalized_shape = torch.Size(normalized_shape)
-
-        assert len(normalized_shape) == 1
-
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.normalized_shape = normalized_shape
-
-    def forward(self, x):
-        mu = x.mean(-1, keepdim=True)
-        sigma = x.var(-1, keepdim=True, unbiased=False)
-        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight + self.bias
-
-
 class LayerNorm(nn.Module):
-    def __init__(self, dim, LayerNorm_type):
+    def __init__(self, dim):
         super(LayerNorm, self).__init__()
-        if LayerNorm_type == 'BiasFree':
-            self.body = BiasFree_LayerNorm(dim)
-        else:
-            self.body = WithBias_LayerNorm(dim)
+        self.body = BiasFree_LayerNorm(dim)
 
     def forward(self, x):
         h, w = x.shape[-2:]
         return to_4d(self.body(to_3d(x)), h, w)
 
+
+class se_block(nn.Module):
+    def __init__(self, inplanes, reduction=16):
+        super(se_block, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(inplanes, inplanes//reduction, 1, 1, 0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inplanes // reduction, inplanes, 1, 1, 0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        input = x
+        x = self.se(x)
+        return input*x
 
 class se_block(nn.Module):
     def __init__(self, inplanes, reduction=16):
